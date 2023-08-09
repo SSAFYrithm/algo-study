7275b24b9fce56b31f93759477713750adaf6104
joonbum <1996joon@gmail.com>
2023-08-09 18:04:27 +0900

    Create 미생물격리.java 구현중

---
+++ week02/joonbum/SWEA2382_미생물격리.java
@@ -0,0 +1,32 @@
+package week02;
+import java.util.*;
+import java.io.*;
+//메모리 : 
+//실행시간 : 
+public class SWEA2382_미생물격리 {
+	public static int[][][] board;
+	public static int[] dy = {1,-1,0,0}; //상하좌우
+	public static int[] dx = {0,0,-1,1};
+	public static int t, n, m, k;
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		t = Integer.parseInt(br.readLine());
+		
+		for(int test_case = 1; test_case <= t; test_case++) {
+//			n = s
+		}
+		
+	}
+
+}
+
+//
+class Mesengmul{
+	int num;
+	int x;
+	int y;
+	int dir;
+}
