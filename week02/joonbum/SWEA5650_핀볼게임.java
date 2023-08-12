fe66cb5b656992cad45e40dfb6e6764839f1be25
bum19 <1996joon@gmail.com>
2023-08-12 12:22:33 +0900

    Update: 핀볼게임 구현완료.java

--- week02/joonbum/SWEA5650_핀볼게임.java
+++ week02/joonbum/SWEA5650_핀볼게임.java
@@ -1,80 +1,85 @@
 package week02;
 
-//메모리	:
-//실행시간 :
+//메모리	: 42,628 kb
+//실행시간 : 933 ms
 import java.util.*;
 import java.io.*;
 
 public class SWEA5650_핀볼게임 {
-	public static int t, n, cnt, maxCnt, startx, starty, dir;
-	public static int[][] board;
-	public static int[][][] warmhole; // 1번째 인덱스 웜홀 번호, 3번째 0인덱스 y좌표, 3번째 1인덱스 x좌표
+	public static int t, n, maxCnt; // 테스트케이스, 게임판크기, 핀볼최댓값
+	public static int[][] board;// 게임판 담을 배열
+	public static int[][][] warmhole; // 웜홀담을 배열. 1번째 인덱스 웜홀 번호, 2번째 인덱스 웜홀 쌍, 3번째 인덱스  y,x좌표
 	public static int[] dy = { -1, 1, 0, 0 }; // 상하좌우
-	public static int[] dx = { 0, 0, -1, 1 }; 
-
+	public static int[] dx = { 0, 0, -1, 1 }; // 상하좌우
+	
 	public static void main(String[] args) throws IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 
-		t = Integer.parseInt(br.readLine());
+		t = Integer.parseInt(br.readLine().trim()); //trim안쓸경우 NumberFormatException 발생.
 
 		for (int test_case = 1; test_case <= t; test_case++) {
-			n = Integer.parseInt(br.readLine());
-			board = new int[n + 2][n + 2]; //입력인덱스시작이 1~n이고, 벽에 부딪히는 로직을 위해 배열전체를 0으로 감싸준다.
-			warmhole = new int[11][2][2]; // 1번째 인덱스 웜홀 번호, 3번째 인덱스 0,1 좌표. warmhole[6][1][0] : 6번웜홀 쌍중 첫번째 웜홀의 y좌표.
-			cnt = 0;
-			maxCnt = Integer.MIN_VALUE;
+			
+			//입력시작
+			n = Integer.parseInt(br.readLine().trim());
+			board = new int[n + 2][n + 2]; //입력인덱스시작이 1~n이고, 
+			for(int i = 0 ; i < n+2; i++) { //벽에 부딪히는 로직을 위해 배열전체를 5로 감싸준다.
+				board[0][i] = 5;
+				board[i][0] = 5;
+				board[n+1][i] = 5;
+				board[i][n+1] = 5;
+			}
+			warmhole = new int[11][2][2]; // [11] : 웜홀번호. 0~5까지는 안쓰고, 6~11만쓴다.	// [2] : 웜홀쌍 0,1  //[2] : y,x좌표 
+			maxCnt = Integer.MIN_VALUE;	//테스트케이스별 최댓값 초기화.
 			for (int i = 1; i <= n; i++) {
-				st = new StringTokenizer(br.readLine());
+				st = new StringTokenizer(br.readLine().trim());
 				for (int j = 1; j <= n; j++) {
 					int tmp = Integer.parseInt(st.nextToken());
-					if (6 <= tmp && tmp <= 10) {
+					if (6 <= tmp && tmp <= 10) { //웜홀 입력
 						int warmIdx = 0; // 웜홀 쌍 중 첫번째인지 두번째인지
-						if (warmhole[tmp][warmIdx][0] != 0)
-							warmIdx++;// 웜홀 입력받은적 있는지 없는지확인.
+						if (warmhole[tmp][warmIdx][0] != 0) //해당 웜홀번호 입력받은적있으면, 1번재 쌍에 저장.
+							warmIdx++;
 						warmhole[tmp][warmIdx][0] = i;
 						warmhole[tmp][warmIdx][1] = j;
-//						System.out.println("warmhole["+tmp+"]["+warmIdx+"][0] : "+i);
-//						System.out.println("warmhole["+tmp+"]["+warmIdx+"][1] : "+j);
 					}
 					board[i][j] = tmp;
 				}
 			}
-//게임판 위에 0,0인곳에서 시작.
+			//입력끝
+			
+			//게임판 위에 0,0인곳에서 시작. n*n 각칸을 순회하며, 0일경우에 이동 시작.
 			for (int i = 1; i <= n; i++) {
 				for (int j = 1; j <= n; j++) {
 					if (board[i][j] != 0)
 						continue;
-					starty = i;
-					startx = j;
 					move(i, j);
 				}
 			}
+			
+			//출력 저장.
 			sb.append("#").append(test_case).append(" ").append(maxCnt).append("\n");
 		}
-		System.out.println(sb);
+		
+		//출력
+		System.out.print(sb);
 	}
 
+	//핀볼 움직이는 메소드. 시작좌표를 인자로 받는다.
 	public static void move(int y, int x) {
-		Queue<int[]> q = new LinkedList<>();
-		
-
+		int cnt = 0, dir;
 		for(int i = 0 ; i < 4; i++) {
 			dir = i;
 //			System.out.println("(y, x) :" + "("+y+", "+x+")");
-			q.offer(new int[] {y,x});
+			int ny = y + dy[dir];
+			int nx = x + dx[dir];
 			//탐색
-			while(!q.isEmpty()) {
-				int[] current = q.poll();
-				
-				int ny = current[0] + dy[dir];
-				int nx = current[1] + dx[dir];
-//				System.out.println("작업전current 위치 : ("+current[0]+", "+current[1]+")"+", dir : "+dir+" ny,nx : ("+ny+", "+nx+")");
-//				if(ny == 0 && nx == 10) System.exit(0);
-//				if(ny == 0 && nx == 0) System.exit(0);;
-				if(ny == 0 || nx == 0 || ny == n+1 || nx == n+1 || board[ny][nx] == 5) { // 벽이거나 5번블록일 때
-//					System.out.println("벽");
+			while(true) {
+				if(board[ny][nx] == -1 || (ny == y && nx ==x) ) { // 종료 조건. 시작좌표로 돌아오거나 블랙홀을 만날경우.
+					break;
+				}
+				else if(board[ny][nx] == 5) { // 벽이거나 5번블록일 때
 					cnt++;
 					if(dir == 0)	  dir = 1;
 					else if(dir == 1) dir = 0;
@@ -82,7 +87,6 @@ public class SWEA5650_핀볼게임 {
 					else if(dir == 3) dir = 2;
 				}
 				else if(board[ny][nx] == 1) {// 1번블록
-//					System.out.println("1블록");
 					cnt++;
 					if(dir == 1) 	  dir = 3;
 					else if(dir == 2) dir = 0;
@@ -90,7 +94,6 @@ public class SWEA5650_핀볼게임 {
 					else if(dir == 0) dir = 1;
 				}
 				else if(board[ny][nx] == 2) {// 2번블록
-//					System.out.println("2블록");
 					cnt++;
 					if(dir == 0)	  dir = 3;
 					else if(dir == 2) dir = 1;
@@ -98,7 +101,6 @@ public class SWEA5650_핀볼게임 {
 					else if(dir == 3) dir = 2;
 				}
 				else if(board[ny][nx] == 3) { // 3번블록
-//					System.out.println("3블록");
 					cnt++;
 					if(dir == 3)	  dir = 1;
 					else if(dir == 0) dir = 2;
@@ -106,36 +108,30 @@ public class SWEA5650_핀볼게임 {
 					else if(dir == 2) dir = 3;
 				}
 				else if(board[ny][nx] == 4 ) { // 4번블록
-//					System.out.println("4블록");
 					cnt++;
 					if(dir == 3)	  dir = 0;
 					else if(dir == 1) dir = 2;
 					else if(dir == 0) dir = 1;
 					else if(dir == 2) dir = 3;
 				}
-				else if(board[ny][nx] == -1 || (ny == y && nx ==x) ) { // 종료 조건
-//					System.out.println("탈출");
-					break;
-				}
-				else if(board[ny][nx] >= 6 && board[ny][nx] <= 10){ //6~10일때
-//					System.out.println("위잉웜홀");
+				else if(board[ny][nx] >= 6 && board[ny][nx] <= 10){ //웜홀 만났을때
 					int tmp = board[ny][nx];
 					int warmIdx = 0;
-					if(warmhole[tmp][warmIdx][0] == ny && warmhole[tmp][warmIdx][1] == nx)// 현재 인덱스가 웜홀 다른 쌍이 6~10이 아니라면
+					if(warmhole[tmp][warmIdx][0] == ny && warmhole[tmp][warmIdx][1] == nx) //현재 웜홀말고 페어 웜홀 좌표찾기. 
 						warmIdx++;
-					ny = warmhole[tmp][warmIdx][0];
-					nx = warmhole[tmp][warmIdx][1];
+					ny = warmhole[tmp][warmIdx][0];		//페어 웜홀 좌표로 y갱신
+					nx = warmhole[tmp][warmIdx][1];		//페어 웜홀 좌표로 x갱신
 				}
-				else {;}
 				
-				//이동 큐에 넣기
-//				System.out.println("작업후current 위치 : ("+current[0]+", "+current[1]+")"+", dir : "+dir+" ny,nx : ("+ny+", "+nx+")");
-				q.offer(new int[] {ny,nx});
+				//다음방향으로 이동
+				ny = ny + dy[dir];
+				nx = nx + dx[dir];
 			}
-			//비교
+			
+			//현재cnt값과 maxCnt값 비교
 			maxCnt = Math.max(maxCnt, cnt);
+			//cnt 탐색끝나면 초기화.
 			cnt = 0;
 		}
-
 	}
 }
