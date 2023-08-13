77297d0a37ea20cc8f57654d072d7aa3b7855697
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-13 22:07:20 +0900

    Create 핀볼 게임.java

---
+++ week02/핀볼 게임.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+	static int N, max, board[][];
+	static int wormhole[][] = new int[5][5];
+	static int[] dx = { -1, 1, 0, 0 };
+	static int[] dy = { 0, 0, -1, 1 };
+	static int[][] block = { { 1, 3, 0, 2 }, { 3, 0, 1, 2 }, { 2, 0, 3, 1 }, { 1, 2, 3, 0 }, { 1, 0, 3, 2 } };
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine().trim());
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(st.nextToken());
+		for (int tc = 1; tc <= T; tc++) {
+			N = Integer.parseInt(br.readLine().trim());
+			board = new int[N][N];
+			max = 0;
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine().trim());
+				for (int j = 0; j < N; j++) {
+					int input = Integer.parseInt(st.nextToken());
+					board[i][j] = input;
+					if (input >= 6 && input <= 10) { // 웜홀정보 저장  
+						int wh[] = wormhole[input - 6];
+						wh[(wh[4]++) % 4] = i;
+						wh[(wh[4]++) % 4] = j;
+					}
+				}
+			}
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					for (int d = 0; d < 4; d++) {
+						if (board[i][j] == 0) {
+							max = Math.max(max, play(i, j, d)); // 시뮬레이션 부분  
+						}
+					}
+				}
+			}
+			sb.append("#").append(tc).append(" ").append(max).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+	public static int play(int r, int c, int d) {
+		int p = 0;
+		int sr = r;
+		int sc = c;
+		while (true) {
+			r += dy[d];
+			c += dx[d];
+			if (r < 0 || r >= N || c < 0 || c >= N) { // 벽에 부딪힌 경우. 왔던 길을되돌아 간다. 
+				return p * 2 + 1;
+			}
+			int n = board[r][c];
+			if (r == sr && c == sc || n == -1) { // 게임 종료조건 -> 제자리 or 블랙홀  
+				return p;
+			} else if (n == 0) {// 빈 공
+				continue;
+			} else if (n <= 5) {// 블록을 만났을 때
+				p++;
+				d = block[n - 1][d];
+
+			} else if (n >= 6 && n <= 10) { // 웜홀을 만났을 때
+				int[] wh = wormhole[n - 6];
+				if (r == wh[0] && c == wh[1]) {
+					r = wh[2];
+					c = wh[3];
+				} else {
+					r = wh[0];
+					c = wh[1];
+
+				}
+
+			}
+
+		}
+
+	}
+
+}
