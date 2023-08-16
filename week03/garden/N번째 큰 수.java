012a97d028ee99e8cff8adbf63bae798f27449aa
SSAFY <SSAFY@DESKTOP-MDJG1NJ>
2023-08-16 09:13:32 +0900

    N번째 큰 수 - solved

---
+++ week03/garden/N번째 큰 수.java
@@ -0,0 +1,30 @@
+package BOJ.Silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+// 메모리 334568kb 시간 892ms
+public class Q2075_N번째큰수 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		// 우선순위 큐를 int의 역순으로 정렬한다
+		Queue<Integer> que = new PriorityQueue<Integer>(Collections.reverseOrder());
+		// 표를 입력받으면 큐에 넣는다
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++)
+				que.offer(Integer.parseInt(st.nextToken()));
+		}
+		// 큐에서 N-1개를 빼내면 다음 순서는 N번째 수가 된다
+		for (int i = 0; i < N - 1; i++)
+			que.poll();
+		System.out.println(que.poll());
+	}
+}
