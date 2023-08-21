e62cd04a4e3767b5045f2915b1ef9000f46da301
SIEUN KIM <sieun043@gmail.com>
2023-08-21 11:12:34 +0900

    0821 특이한자석

--- week04/sieun/특이한자석.java
+++ week04/sieun/특이한자석.java
@@ -1,5 +1,8 @@
 package sieun;
-
+/*
+104ms
+18636KB
+*/
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
@@ -10,12 +13,13 @@ import java.util.StringTokenizer;
 
 public class 특이한자석 {
 
-	private static int[][] gear;
-	private static int[] top;
+	private static int[][] gear; // 톱니바퀴
+	private static int[] top; // 맨 위로 올라온 톱니의 번호
 	private static int[] score = {1, 2, 4, 8};
 
 	public static void main(String[] args) throws IOException {
-		//System.setIn(new FileInputStream("./res/input01.txt"));
+		// System.setIn(new FileInputStream("./res/input01.txt"));
+		// 입출력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder answer = new StringBuilder();
@@ -49,7 +53,7 @@ public class 특이한자석 {
 		bw.close();
 	}
 
-	private static void rotate(int g, int d) {
+	private static void rotate(int g, int d) { // g번째 톱니를 d방향으로 회전
 		if(g>0) {
 			rotateL(g-1, d*(-1));
 		}
@@ -59,7 +63,7 @@ public class 특이한자석 {
 		turn(g, d);
 	}
 
-	private static void rotateL(int g, int d) {
+	private static void rotateL(int g, int d) { // g번째 톱니가 돌아갈 수 있다면 d방향으로 돔
 		boolean flag = false;
 		int gNum1 = top[g+1]-2, gNum2 = top[g]+2;
 		if(gNum1<0) {
@@ -78,7 +82,7 @@ public class 특이한자석 {
 			turn(g, d);
 	}
 
-	private static void rotateR(int g, int d) {
+	private static void rotateR(int g, int d) { // g번째 톱니가 돌수 있으면 d방향으로 돔
 		boolean flag = false;
 		int gNum1 = top[g-1]+2, gNum2 = top[g]-2;
 		if(gNum2<0) {
